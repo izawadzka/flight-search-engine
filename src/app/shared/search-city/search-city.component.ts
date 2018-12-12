ef41a4a6fd206c8c05491bde65f43271e5507952
Izabela <i.k.zawadzka@gmail.com>
2018-12-12 07:43:10 +0100

    Format data with pipes instead of functions

--- src/app/shared/search-city/search-city.component.ts
+++ src/app/shared/search-city/search-city.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatSort } from '@angular/material';
 import { Flight } from '../models/flight';
 import { ServerResponse } from '../server-response';
-import { Airport } from '../models/airport';
 
 @Component({
   selector: 'search-airport-component',
@@ -25,18 +24,5 @@ export class SearchCityComponent{
       this.dataSource = new MatTableDataSource(serverResponse.getValues());
       this.dataSource.sort = this.sort;
     }  //todo: else
-  }
-
-  getAirportDescription(airport: Airport){
-    let description = "";
-    if(airport != null){
-      description += airport.name && airport.name.length > 0 ? `${airport.name} in` : "";
-      description += airport.city && airport.city.length > 0 ? `${airport.city},` : "";
-      description += airport.country && airport.country.length > 0 ? airport.country : ""; 
-    }else{
-      description = "not specified";
-    }
-
-    return description;
   }
 }
--- src/app/shared/search-flights-component/search-flights.component.ts
+++ src/app/shared/search-flights-component/search-flights.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { Flight } from '../models/flight';
 import { ServerResponse } from '../server-response';
 import {MatSort, MatTableDataSource} from '@angular/material';
-import { Airport } from '../models/airport';
 
 
 @Component({
@@ -23,18 +22,5 @@ export class SearchFlightsComponent{
       this.dataSource = new MatTableDataSource(serverResponse.getValues());
       this.dataSource.sort = this.sort;
     }  //todo: else
-  }
-
-  getAirportDescription(airport: Airport){
-    let description = "";
-    if(airport != null){
-      description += airport.name && airport.name.length > 0 ? `${airport.name} in` : "";
-      description += airport.city && airport.city.length > 0 ? `${airport.city},` : "";
-      description += airport.country && airport.country.length > 0 ? airport.country : ""; 
-    }else{
-      description = "not specified";
-    }
-
-    return description;
   }
 }
